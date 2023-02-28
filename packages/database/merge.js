e23d694a89914ac64ba5912b0c9677ddd68a0514
unsphere <apps@unsphere.net>
2023-02-28 21:30:10 +0100

    add prisma-merge script

---
+++ packages/database/merge.js
@@ -0,0 +1,67 @@
+const { glob } = require('glob');
+const fs = require('fs');
+
+const config = `
+generator client {
+    provider        = "prisma-client-js"
+    previewFeatures = ["multiSchema"]
+}
+  
+datasource db {
+    provider = "postgresql"
+    url      = env("DATABASE_URL")
+    schemas  = ["auth", "public"]
+}
+`;
+
+const readFile = (path, data, opts = 'utf8') =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path, opts, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+
+const writeFile = (path, data, opts = 'utf8') =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(path, data, opts, (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+
+const main = async () => {
+  console.log('Reading files...');
+  const data = [];
+  const filePaths = await glob(['./prisma/**/*.prisma']);
+  console.log(filePaths);
+  console.log('Merging files...');
+  for (let i = 0; i < filePaths.length; i++) {
+    const filePath = filePaths[i];
+    if (filePath.indexOf('schema.prisma') < 0) {
+      const file = (await readFile(filePath))
+        .split('\n')
+        .filter((line) => line.indexOf('import') < 0)
+        .join('\n')
+        .replace(/generator(.*?)}\n\n/s, '');
+      if (filePath.indexOf('supabase.prisma') > -1) {
+        data.unshift(
+          `\n//BEGIN ${filePath.replace('prisma/', '')}\n` +
+            file +
+            `\n//END ${filePath.replace('prisma/', '')}\n`
+        );
+      } else {
+        data.push(
+          `\n//BEGIN ${filePath.replace('prisma/', '')}\n` +
+            file +
+            `\n//END ${filePath.replace('prisma/', '')}\n`
+        );
+      }
+    }
+  }
+  data.unshift(config);
+  await writeFile('prisma/schema.prisma', data.join('\n'));
+  console.log('Merged successfully!');
+};
+
+main();
