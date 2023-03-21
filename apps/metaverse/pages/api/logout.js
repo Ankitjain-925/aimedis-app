83b5f444c5a7f5bd11fd1d8a24df9cddb0051d23
Shivam Tiwari <shivamtiwari100@gmail.com>
2023-03-21 17:24:22 +0400

    Token Validation After Logging out

--- apps/metaverse/pages/api/logout.js
+++ apps/metaverse/pages/api/logout.js
@@ -1,9 +1,25 @@
 import { UserAuthorization } from 'database/utils/user-auth';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseServer = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_SECRET
+);
 
 export default UserAuthorization(async function logout(req, res) {
-    const { error } = await req.supabase.auth.api.signOut(req.token);
-    if (error ) {
+    const { error } = await req.supabase.auth.signOut(req.token);
+    if (error) {
       return res.status(500).json({ status: 500, message: error.message });
+    }
+    // Insert the invalidated token into the "invalidated_tokens" table
+    const { error: insertError } = await supabaseServer.from('invalidated_tokens').insert([{ token: req.token }]);
+
+    if (insertError) {
+    return res.status(500).json({ status: 500, message: insertError.message });
     }
     return res.status(200).json({ message: 'Logged out successfully' });
   });
+
+
+  
+  
