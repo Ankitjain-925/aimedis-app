eff76be890830471d24fbf334ff2b74c53beee31
Sameer Alam <114988871+sameermedis@users.noreply.github.com>
2023-03-27 14:14:18 +0530

    supabase error fix

--- apps/health/pages/_app.js
+++ apps/health/pages/_app.js
@@ -1,11 +1,14 @@
-import { SupabaseProvider, ChakraProvider } from 'ui';
+import {   DatabaseProvider,
+  UserProvider, ChakraProvider } from 'ui';
 
 export default function App({ Component, pageProps }) {
   return (
-    <SupabaseProvider pageProps={pageProps}>
-      <ChakraProvider pageProps={pageProps}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SupabaseProvider>
+    <DatabaseProvider pageProps={pageProps}>
+      <UserProvider>
+        <ChakraProvider pageProps={pageProps}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </UserProvider>
+    </DatabaseProvider>
   );
 }
